71fe2693fa8cf0b09870c036265b2e87ce984ba7
Daniel Amaral <silva.daniel86@Gmail.com>
2021-10-26 12:59:36 -0300

    add globals and improve dan_theme

--- R/dan_theme.R
+++ R/dan_theme.R
@@ -2,24 +2,26 @@
 #'
 #' Minimal ggplot2 theme using the Poppins and Roboto Mono fonts
 #'
-#' @param ggp_object ggplot2 object
 #' @return ggplot2 custom theme
 #' @export
-#' @importFrom sysfonts font_add_google
+#' @importFrom sysfonts font_add_google font_families
 #' @importFrom showtext showtext_auto
-#' @importFrom ggplot2 theme_light theme element_text element_blank
-dan_theme <- function(ggp_object) {
-  font_add_google("Poppins", "Poppins")
-  font_add_google("Roboto Mono", "Roboto Mono")
+#' @importFrom purrr walk
+#' @importFrom glue glue
+#' @importFrom ggplot2 theme_light theme element_text element_blank labs
+dan_theme <- function() {
+  fonts <- c("Poppins", "Roboto Mono")
+  walk(fonts, \(fnt) {
+    if(!(fnt %in% font_families())) font_add_google(fnt, fnt)
+  })
   showtext_auto()
 
-  ggp_object +
-    theme_light(base_size = 18, base_family = "Poppins") +
-    theme(
-      legend.position = "bottom",
-      axis.title = element_text(size = 14),
-      axis.text.x = element_text(family = "Roboto Mono", size = 11),
-      plot.caption = element_text(size = 12, color = "gray40"),
-      panel.grid = element_blank()
-    )
+  theme_light(base_size = 18, base_family = fonts[1]) +
+  theme(
+    legend.position = "bottom",
+    axis.title = element_text(size = 14),
+    axis.text.x = element_text(family = fonts[2], size = 11),
+    plot.caption = element_text(size = 12, color = "gray40"),
+    panel.grid = element_blank()
+  )
 }
