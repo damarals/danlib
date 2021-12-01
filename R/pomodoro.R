625290d0cc0471e2ef0092f7e186b8b8ae453d9d
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-01 00:10:54 -0300

    :rocket: v1 of pomodoro

--- R/pomodoro.R
+++ R/pomodoro.R
@@ -6,22 +6,21 @@
 #' @param time_focus focus time in minutes
 #' @param time_break break time in minutes
 #' @export
-#' @importFrom beepr beep
-#' @importFrom glue glue
-#' @importFrom rstudioapi showDialog
-#' @importFrom job job
-pomodoro <- function(time_focus = 25, time_break = 5) {
-  suppressMessages(
-    job(pomodoro = {
-      while(TRUE) {
-        Sys.sleep(60 * {time_focus}) # focus time
-        counter <- counter + 1
-        beep(1)
-        showDialog('Pomodoro timer', 'Pomodoro period finished! Time to take a break!')
-        Sys.sleep(60 * {time_break}) # break time
-        beep(5)
-        showDialog('Pomodoro timer', 'Time to get back to work!')
-      }
-    })
-  )
+#' @importFrom rstudioapi jobRunScript executeCommand
+pomodoro <- function(time_focus = 25, short_break = 5, long_break = 15) {
+  code <- paste0("
+    counter <- 0
+    while(TRUE) {
+        Sys.sleep(60 * ", time_focus, ")
+        beepr::beep(1)
+        rstudioapi::showDialog('Pomodoro timer', 'Pomodoro period finished! Time to take a break!')
+        time_break <- ifelse(counter %% 4, ", short_break, ", ", long_break, ")
+        Sys.sleep(60 * time_break) # break time
+        beepr::beep(5)
+        rstudioapi::showDialog('Pomodoro timer', 'Time to get back to work!')
+    }")
+  script_file <- tempfile()
+  write(code, file = script_file)
+  job <- jobRunScript(script_file, "pomodoro")
+  console <- executeCommand("activateConsole")
 }
