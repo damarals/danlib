623d64dbce7afd408ab5f49e320da10370cf88d4
Daniel Amaral <silva.daniel86@Gmail.com>
2022-05-12 13:26:14 -0300

    :rocket: new parameters dan_theme

--- R/dan_theme.R
+++ R/dan_theme.R
@@ -8,19 +8,20 @@
 #' @importFrom showtext showtext_auto
 #' @importFrom purrr walk
 #' @importFrom ggplot2 theme_light theme element_text element_blank
-dan_theme <- function() {
+dan_theme <- function(base_size = 18, axis_title_size = 14, axis_text_x_size = 11,
+                      caption_plot_size = 12) {
   fonts <- c("Poppins", "Roboto Mono")
   walk(fonts, \(fnt) {
     if(!(fnt %in% font_families())) font_add_google(fnt, fnt)
   })
   showtext_auto()
 
-  theme_light(base_size = 18, base_family = fonts[1]) +
+  theme_light(base_size = base_size, base_family = fonts[1]) +
   theme(
     legend.position = "bottom",
-    axis.title = element_text(size = 14),
-    axis.text.x = element_text(family = fonts[2], size = 11),
-    plot.caption = element_text(size = 12, color = "gray40"),
+    axis.title = element_text(size = axis_title_size),
+    axis.text.x = element_text(family = fonts[2], size = axis_text_x_size),
+    plot.caption = element_text(size = caption_plot_size, color = "gray40"),
     panel.grid = element_blank()
   )
 }
